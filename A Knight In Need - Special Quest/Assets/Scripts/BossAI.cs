d57045e8a717550f43f87569813cb2d0ba189728
Crashfan1ooo <saraviagio1@yahoo.com>
2022-05-13 20:52:25 -0400

    Added combat logic to Boss

--- A Knight In Need - Special Quest/Assets/Scripts/BossAI.cs
+++ A Knight In Need - Special Quest/Assets/Scripts/BossAI.cs
@@ -10,6 +10,11 @@ public class BossAI : MonoBehaviour
     public GameObject player;
     private Transform targetTransform;
 
+    public int bossHitPoints = 30;  //This is for when the sword collides, damage is done in increments of 3
+    public WeaponController playerSword;  //This is for a reference to the sword
+    public Player thePlayer; //Reference to the player itself
+    public bool coolingDown = false;
+
 
     void Start()
     {
@@ -23,6 +28,38 @@ public class BossAI : MonoBehaviour
 
         navMeshAgent.destination = targetTransform.position;
 
+        if (bossHitPoints <= 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("Boss dead af");
+        }
+    }
 
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Sword" && playerSword.isAttacking == true)
+        {
+            bossHitPoints--;
+            Debug.Log("Boss taking damage...");
+        }
+
+        if (other.tag == "Player")
+        {
+            if (coolingDown == false)
+            {
+                thePlayer.Health =  thePlayer.Health - 1;
+                coolingDown = true;
+                Debug.Log("Player taking damage...");
+                StartCoroutine(AttackCoolDown(3.0f));
+
+
+            }
+        }
+    }
+
+    private IEnumerator AttackCoolDown(float waittime)
+    {
+        yield return new WaitForSeconds(waittime);
+        coolingDown = false;
     }
 }
